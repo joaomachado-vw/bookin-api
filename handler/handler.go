9529b3678cce2cee41da6c6d25aae7e29c826a43
Joao Machado <joao.machado@volkswagen-groupservices.com>
2024-04-25 19:20:19 +0200

    New functions created to post and multiple post

--- handler/handler.go
+++ handler/handler.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-type BookingRequest struct {
+type BookingRequestJSON struct {
 	RequestID   string  `json:"request_id"`
 	CheckIn     string  `json:"check_in"`
 	Nights      int     `json:"nights"`
@@ -13,25 +13,56 @@ type BookingRequest struct {
 	Margin      float64 `json:"margin"`
 }
 
-var bookings []BookingRequest
+var bookings []BookingRequestJSON
 
 func BookingHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(bookings)
 	}
+}
 
+func BookingRequestHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		var bookingRequest BookingRequest
+		var bookingRequest BookingRequestJSON
 		w.Header().Set("Content-Type", "application/json")
 		err := json.NewDecoder(r.Body).Decode(&bookingRequest)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
+		ValidationErrorResponse(w, r, bookingRequest)
 		bookings = append(bookings, bookingRequest)
 		if err := json.NewEncoder(w).Encode(bookingRequest); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
+	}
+}
+
+func BookingRequestListHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		var bookingRequests []BookingRequestJSON
+		w.Header().Set("Content-Type", "application/json")
+		err := json.NewDecoder(r.Body).Decode(&bookingRequests)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		for _, bookingRequest := range bookingRequests {
+			ValidationErrorResponse(w, r, bookingRequest)
+			bookings = append(bookings, bookingRequest)
+		}
+		if err := json.NewEncoder(w).Encode(bookingRequests); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
+
+// ESTA ES LA MEJOR FORMA DE HACER ESTO?!?!
+func ValidationErrorResponse(w http.ResponseWriter, r *http.Request, bookingRequest BookingRequestJSON) {
+	if (bookingRequest.RequestID == "" && bookingRequest.Nights == 0 && bookingRequest.Margin == 0 &&
+		bookingRequest.CheckIn == "" && bookingRequest.SellingRate == 0) || r.Body == nil {
+		http.Error(w, "All fields are mandatory", http.StatusBadRequest)
+		return
 	}
 }
--- handler/handler_test.go
+++ handler/handler_test.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"bytes"
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -20,6 +21,40 @@ func TestHandlerPOST(t *testing.T) {
 	body := []byte(expectedBody)
 	req, _ := http.NewRequest("POST", "/stats", bytes.NewBuffer(body))
 	response := httptest.NewRecorder()
-	BookingHandler(response, req)
-	assert.Equal(t, expectedBody+"\n", response.Body.String())
+	BookingRequestHandler(response, req)
+	var actualBooking BookingRequestJSON
+	json.Unmarshal(response.Body.Bytes(), &actualBooking)
+	var expectedBooking BookingRequestJSON
+	json.Unmarshal([]byte(expectedBody), &expectedBooking)
+	assert.Equal(t, expectedBooking, actualBooking)
+}
+
+func TestBookingRequestListHandler(t *testing.T) {
+	expectedBody := `
+[
+  {
+    "request_id": "123",
+    "check_in": "2024-04-29",
+    "nights": 2,
+    "selling_rate": 50,
+    "margin": 20
+  },
+  {
+    "request_id": "456",
+    "check_in": "2024-05-01",
+    "nights": 5,
+    "selling_rate": 30,
+    "margin": 30
+  }
+]
+`
+	body := []byte(expectedBody)
+	req, _ := http.NewRequest("POST", "/stats", bytes.NewBuffer(body))
+	response := httptest.NewRecorder()
+	BookingRequestListHandler(response, req)
+	var actualBookings []BookingRequestJSON
+	json.Unmarshal(response.Body.Bytes(), &actualBookings)
+	var expectedBookings []BookingRequestJSON
+	json.Unmarshal([]byte(expectedBody), &expectedBookings)
+	assert.Equal(t, expectedBookings, actualBookings)
 }
